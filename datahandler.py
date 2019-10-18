39c6a0c93061db3302cd85c55b9a701bfa0fcb3a
benoitgaujac <benoit.gaujac@gmail.com>
2019-10-18 12:54:32 +0100

    loading pb allow_pickle

--- datahandler.py
+++ datahandler.py
@@ -432,7 +432,7 @@ class DataHandler(object):
         logging.error('Loading dsprites')
         data_dir = _data_dir(opts)
         data_file = os.path.join(data_dir, 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
-        X = np.load(data_file)['imgs']
+        X = np.load(data_file,allow_pickle=True)['imgs']
         X = X[:, :, :, None]
 
         seed = 123
