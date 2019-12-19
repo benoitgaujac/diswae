0e6dee3cdc97eac34c3147f1c09bd7bccc73d0fd
benoitgaujac <benoit.gaujac@gmail.com>
2019-12-19 15:07:53 +0000

    plotting typo

--- run_array.py
+++ run_array.py
@@ -157,8 +157,7 @@ def main():
 
     # Experiemnts set up
     opts['epoch_num'] = int(FLAGS.num_it / int(data.num_points/opts['batch_size']))
-    opts['plot_every'] = int(opts['epoch_num'] / 3.) * int(data.num_points/opts['batch_size'])-1
-    opts['evaluate_every'] = int(opts['plot_every'] / 2)
+    opts['evaluate_every'] = int(opts['epoch_num'] / 5.) * int(data.num_points/opts['batch_size'])-1
     opts['save_every'] = 1000000000
     opts['save_final'] = False
     opts['save_train_data'] = True
