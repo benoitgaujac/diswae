b5433c2da5ccd7ab914cc1a8dd7a4974e7a047d3
benoitgaujac <benoit.gaujac@gmail.com>
2020-10-01 08:29:59 +0100

    wae dsprites

--- run_array.py
+++ run_array.py
@@ -187,7 +187,7 @@ def main():
             if opts['cost'] == 'xentropy':
                 beta = [1, 5, 10, 25, 50, 100]
             else:
-                beta = [1, 2, 4, 6, 8, 10]
+                beta = [1, 5, 10, 25, 50, 100]
             coef_id = (FLAGS.id-1) % len(beta)
             opts['obj_fn_coeffs'] = beta[coef_id]
         else:
